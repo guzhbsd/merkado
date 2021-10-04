81e3079ee2c584b78a2bb8a727de9900b5ffeb2e
kevin <kevin@DESKTOP-JSK02LQ>
2021-10-04 18:22:26 -0500

    [Gestion Disquera] - Buscar Disqueras Activas
    Se crea el metodo en el repositorio para listar las disqueras activas.

--- merkado/src/main/java/com/barrio/merkado/repository/DisqueraRepository.java
+++ merkado/src/main/java/com/barrio/merkado/repository/DisqueraRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.barrio.merkado.entity.Disquera;
 
@@ -14,6 +13,5 @@ public interface DisqueraRepository extends JpaRepository<Disquera, Integer>{
 	
 	public List<Disquera> findByPais(String pais);
 	
-	@Query(value = "from Disquera d where d.activo = 1")
-	public List<Disquera> buscarDisquerasActivas();
+	public List<Disquera> findByActivoTrue();
 }
